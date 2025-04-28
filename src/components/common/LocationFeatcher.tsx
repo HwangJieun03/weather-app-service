510e9ee38f660b5f6373a1fb2fdf44a680436c98
jieun <jieunh0311@gmail.com>
2025-04-28 16:21:42 +0900

    현재 위치 가져오기

---
+++ src/components/common/LocationFeatcher.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect } from "react";
+import Button from "./Button";
+
+interface LocationFetcherProps {
+  onLocationChange: (lat: number, lon: number) => void;
+}
+
+const LocationFetcher: React.FC<LocationFetcherProps> = ({ onLocationChange }) => {
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          onLocationChange(latitude, longitude); 
+        },
+        (error) => {
+          console.error("위치를 가져오는 데 실패했습니다.", error);
+        }
+      );
+    } else {
+      console.log("이 브라우저는 Geolocation을 지원하지 않습니다.");
+    }
+  };
+
+  useEffect(() => {
+    getLocation(); 
+  }, []);
+
+  return <Button onClick={getLocation}>현재 위치 확인</Button>;
+};
+
+export default LocationFetcher;
